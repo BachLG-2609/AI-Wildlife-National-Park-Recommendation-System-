2d27ecc9b6fe0309e1cd8927aa85946b15525246
Lê Gia Bách <bachlegia2609@gmail.com>
2026-08-03 20:48:46 +0700

    bách sửa

--- config/db.js
+++ config/db.js
@@ -1,18 +1,17 @@
-
 import mysql from "mysql2";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'ai_wildlife_national_park_db',
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "ai_wildlife_national_park_db",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  namedPlaceholders: true
+  namedPlaceholders: true,
   port: process.env.DB_PORT,
 });
 
